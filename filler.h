614bdb0926f0c0c2bf2beae26b23d991c0d2422e
ppanchen <mrpeterpan10@gmail.com>
2017-02-10 17:27:02 +0200

    start

--- filler.h
+++ filler.h
@@ -13,5 +13,19 @@
 #ifndef FILLER_H
 # define FILLER_H
 # include "libft/libft.h"
+# include <fcntl.h>   //must be removed
+
+typedef	struct 	s_config
+{
+	int 		col;
+	int 		raw;
+	int 		pnum;
+	char 		symb;
+	char 		**map;
+	int 		p_col;
+	int 		p_raw;
+	char 		*piece;
+}				t_config;
+
 
 #endif
--- main.c
+++ main.c
@@ -12,7 +12,37 @@
 
 #include "filler.h"
 
+char	**parse_map(t_config *conf)
+{
+
+}
+
 int 	main(int ac, char **av)
 {
+	char		*file;
+	int			i;
+	int 		j;
+	int			fd;
+	t_config	*conf;
+
+	fd = open("file.txt", O_CREAT | O_RDWR);
+	conf	= (t_config *)malloc(sizeof(t_config));
+	get_next_line(0, &file);
+	conf->pnum = file[10] - '0';
+	conf->symb = (conf->pnum == 1) ? 'O' : 'X';
+	while (get_next_line(0, &file))
+	{
+		ft_putstr_fd(file, fd);
+		ft_putchar_fd('\n', fd,  -1);
+		i = 0;
+		while (ft_isdigit(file[i]))
+			i++;
+		file += i;
+		conf->raw = ft_atoi(file);
+		file += ft_numlen(conf->raw);
+		conf->col = ft_atoi(file);
+		conf->map = parse_map(conf);
+	}
+	close(fd);
 	return (0);
 }
